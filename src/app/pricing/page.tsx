66f9f7a8d67b4284f612e83e62f45b1972dd6293
Krish Ahlawat <krishahlawat2520@gmail.com>
2025-03-27 20:24:00 +0530

    added mode toggle in pricing

--- src/app/pricing/page.tsx
+++ src/app/pricing/page.tsx
@@ -43,14 +43,16 @@ export default function PricingPage() {
 
   return (
     <>
-      <div className="relative isolate bg-white px-6 py-24 sm:py-32 lg:px-8">
+      <div className="relative isolate bg-white dark:bg-black px-6 py-24 sm:py-32 lg:px-8">
         <div className="mx-auto max-w-4xl text-center">
-          <h2 className="text-base/7 font-semibold text-indigo-600">Pricing</h2>
-          <p className="mt-2 text-5xl font-semibold tracking-tight text-balance text-gray-900 sm:text-6xl">
+          <h2 className="text-base/7 font-semibold text-indigo-600 dark:text-indigo-400">
+            Pricing
+          </h2>
+          <p className="mt-2 text-5xl font-semibold tracking-tight text-balance text-gray-900 dark:text-white sm:text-6xl">
             Choose the right plan for you
           </p>
         </div>
-        <p className="mx-auto mt-6 max-w-2xl text-center text-lg font-medium text-pretty text-gray-600 sm:text-xl/8">
+        <p className="mx-auto mt-6 max-w-2xl text-center text-lg font-medium text-pretty text-gray-600 dark:text-gray-300 sm:text-xl/8">
           Choose an affordable plan that&apos;s packed with the best features
           for engaging your audience, creating customer loyalty, and driving
           sales.
@@ -61,20 +63,22 @@ export default function PricingPage() {
               key={tier.id}
               className={classNames(
                 tier.featured
-                  ? "relative bg-gray-900 shadow-2xl"
-                  : "bg-white/60 sm:mx-8 lg:mx-0",
+                  ? "relative bg-gray-900 dark:bg-gray-800 shadow-2xl"
+                  : "bg-white/60 dark:bg-gray-700 sm:mx-8 lg:mx-0",
                 tier.featured
                   ? ""
                   : tierIdx === 0
                   ? "rounded-t-3xl sm:rounded-b-none lg:rounded-tr-none lg:rounded-bl-3xl"
                   : "sm:rounded-t-none lg:rounded-tr-3xl lg:rounded-bl-none",
-                "rounded-3xl p-8 ring-1 ring-gray-900/10 sm:p-10"
+                "rounded-3xl p-8 ring-1 ring-gray-900/10 dark:ring-gray-700 sm:p-10"
               )}
             >
               <h3
                 id={tier.id}
                 className={classNames(
-                  tier.featured ? "text-indigo-400" : "text-indigo-600",
+                  tier.featured
+                    ? "text-indigo-400"
+                    : "text-indigo-600 dark:text-indigo-400",
                   "text-base/7 font-semibold"
                 )}
               >
@@ -83,7 +87,9 @@ export default function PricingPage() {
               <p className="mt-4 flex items-baseline gap-x-2">
                 <span
                   className={classNames(
-                    tier.featured ? "text-white" : "text-gray-900",
+                    tier.featured
+                      ? "text-white"
+                      : "text-gray-900 dark:text-gray-100",
                     "text-5xl font-semibold tracking-tight"
                   )}
                 >
@@ -91,7 +97,9 @@ export default function PricingPage() {
                 </span>
                 <span
                   className={classNames(
-                    tier.featured ? "text-gray-400" : "text-gray-500",
+                    tier.featured
+                      ? "text-gray-400"
+                      : "text-gray-500 dark:text-gray-400",
                     "text-base"
                   )}
                 >
@@ -100,7 +108,9 @@ export default function PricingPage() {
               </p>
               <p
                 className={classNames(
-                  tier.featured ? "text-gray-300" : "text-gray-600",
+                  tier.featured
+                    ? "text-gray-300"
+                    : "text-gray-600 dark:text-gray-400",
                   "mt-6 text-base/7"
                 )}
               >
@@ -109,20 +119,26 @@ export default function PricingPage() {
               <ul
                 role="list"
                 className={classNames(
-                  tier.featured ? "text-gray-300" : "text-gray-600",
+                  tier.featured
+                    ? "text-gray-300"
+                    : "text-gray-600 dark:text-gray-400",
                   "mt-8 space-y-3 text-sm/6 sm:mt-10"
                 )}
               >
                 {tier.features.map((feature) => (
                   <li key={feature} className="flex gap-x-3">
-                    <CheckIcon
-                      aria-hidden="true"
-                      className={classNames(
-                        tier.featured ? "text-indigo-400" : "text-indigo-600",
-                        "h-6 w-5 flex-none"
-                      )}
-                    />
-                    {feature}
+                    <div className="flex items-center">
+                      <CheckIcon
+                        aria-hidden="true"
+                        className={classNames(
+                          tier.featured
+                            ? "text-indigo-400"
+                            : "text-indigo-600 dark:text-indigo-400",
+                          "h-6 w-5 flex-none"
+                        )}
+                      />
+                      <span>{feature}</span>
+                    </div>
                   </li>
                 ))}
               </ul>
@@ -132,7 +148,7 @@ export default function PricingPage() {
                 className={classNames(
                   tier.featured
                     ? "bg-indigo-500 text-white shadow-xs hover:bg-indigo-400 focus-visible:outline-indigo-500"
-                    : "text-indigo-600 ring-1 ring-indigo-200 ring-inset hover:ring-indigo-300 focus-visible:outline-indigo-600",
+                    : "text-indigo-600 dark:text-indigo-400 ring-1 ring-indigo-200 ring-inset hover:ring-indigo-300 focus-visible:outline-indigo-600",
                   "mt-8 block rounded-md px-3.5 py-2.5 text-center text-sm font-semibold focus-visible:outline-2 focus-visible:outline-offset-2 sm:mt-10"
                 )}
               >

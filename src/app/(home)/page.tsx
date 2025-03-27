4a683da6d4cef7290ff3c5335c2a6aa3b28ee363
Krish Ahlawat <krishahlawat2520@gmail.com>
2025-03-27 13:52:49 +0530

    fixed some bugs

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,14 +1,7 @@
-import React from 'react'
-import Navbar from '../../components/Navbar'
-import Footer from '../../components/Footer'
-
-const HomePage = () => {
+export default function Home() {
   return (
     <div>
-        <Navbar />
-        <Footer/>
+      Krish
     </div>
-  )
+  );
 }
-
-export default HomePage
--- src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import Image from "next/image";
-import HomePage from "../app/(home)/page";
-export default function Home() {
-  return (
-    <div>
-      <HomePage />
-    </div>
-  );
-}

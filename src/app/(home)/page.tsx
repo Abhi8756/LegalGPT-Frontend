e0f7b88cb5e2dba47633ba95a2852a8cdf8a4e62
Krish Ahlawat <krishahlawat2520@gmail.com>
2025-03-27 22:53:44 +0530

    added the feature section

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,9 +1,13 @@
+import FeatureRevealSection from "./featureRevealSection";
+import { FeatureSection } from "./featureSection";
 import { HeroSection } from "./herosection";
 
 export default function Home() {
   return (
     <div>
       <HeroSection />
+      <FeatureRevealSection />
+      <FeatureSection />
     </div>
   );
 }
